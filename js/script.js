baac115e541d6760019483f68ae08601c9d21c79
Oyatunji Adedamola <dedribble@gmail.com>
2021-07-22 21:11:55 +0100

    Fixed accordion functionality

--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
-const arrow = document.querySelectorAll('.arrow');
-const body = document.querySelectorAll('.body')
+const arOne = document.getElementById('arOne');
+const arTwo = document.getElementById('arTwo');
+const arThree = document.getElementById('arThree');
+const arFour = document.getElementById('arFour');
+const arFive = document.getElementById('arFive');
 const header = document.querySelectorAll('.heading');
+const body1 = document.getElementById('body1')
+const body2 = document.getElementById('body2')
+const body3 = document.getElementById('body3')
+const body4 = document.getElementById('body4')
+const body5 = document.getElementById('body5')
 
-arrow.forEach(btn => {
-    btn.addEventListener('click', ()=> {
-        body.forEach(btn => btn.classList.toggle('active'));
-    })
+arOne.addEventListener('click', ()=> {
+
+    body1.classList.toggle('active')
+})
+
+arTwo.addEventListener('click', ()=> {
+    body2.classList.toggle('active')
+    
+})
+
+arThree.addEventListener('click', ()=> {
+    body3.classList.toggle('active')
+})
+
+arFour.addEventListener('click', ()=> {
+    body4.classList.toggle('active')
+})
+
+arFive.addEventListener('click', ()=> {
+    arTwo.style.transform = "rotate(180deg)"
+    body5.classList.toggle('active')
 })
